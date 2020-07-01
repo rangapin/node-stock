dd41792443a4c534170fae22bd63eccc74de0625
Richard Angapin <richard@ngapin.home>
2020-07-01 02:29:07 +0200

    adding hhandlebars

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const exphbs  = require('express-handlebars');
 const path =require('path');
 
 const PORT = process.env.PORT || 5000;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
